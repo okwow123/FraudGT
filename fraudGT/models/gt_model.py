b9fee87bdbd8dcf6e4589ea63d45f176414c5043
jeje <okwow123@naver.com>
2025-07-26 16:52:44 +0900

    Create gt_model.py

---
+++ fraudGT/models/gt_model.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+from fraudGT.layer.gt_layer import GTLayerMEM
+
+class GTModel(nn.Module):
+    def __init__(self, dim_in, dim_out, dataset=None):
+        super().__init__()
+        self.encoder = GTLayerMEM(dim_in, dim_out, dataset)
+    
+    def forward(self, batch):
+        return self.encoder(batch)

c8476b3b060aec7cca530f7e48abb30e8f3021e0
jeje <okwow123@naver.com>
2025-08-29 10:17:08 +0900

    Update gt_config.py

--- fraudGT/config/gt_config.py
+++ fraudGT/config/gt_config.py
@@ -101,6 +101,4 @@ def set_cfg_gt(cfg):
     # cfg.gt.bigbird.block_size = 3
 
     # cfg.gt.bigbird.layer_norm_eps = 1e-6
-    cfg.data.edge_mask_ratio = 0.15   
-    cfg.data.node_mask_ratio = 0.0    
 
